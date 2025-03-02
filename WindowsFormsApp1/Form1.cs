26b0ef6f6fac73483ea5bf9dda3af43b091e0b8c
AleksndrSakharov <mr.11aleksandrsasharov11@mail.ru>
2025-03-02 16:09:31 +0300

    s

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,9 +12,64 @@ namespace WindowsFormsApp1
 {
     public partial class Form1: Form
     {
+        Bitmap image;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files | *.png; *.jpg; *.bmp; | All files (*.*) | *.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                image = new Bitmap(dialog.FileName);
+                pictureBox1.Image = image;
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void инверсияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InvertFilter filter = new InvertFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Bitmap newImage = ((Filters)e.Argument).processImage(image, backgroundWorker1);
+            if (backgroundWorker1.CancellationPending != true) image = newImage;
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (!e.Cancelled)
+            {
+                pictureBox1.Image = image;
+                pictureBox1.Refresh();
+            }
+            progressBar1.Value = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
         }
     }
 }
